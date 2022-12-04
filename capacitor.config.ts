1f67b40f17f3f7643d1c8fb26f74431231292dd5
Pablo Fernandez <pabloubeda2000@gmail.com>
2022-12-04 17:08:54 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ig.recipes',
+  appName: 'ig-recipes',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

a65439c02e53536c639940605b8db2a8c8a310c9
Pablo Fernandez <pabloubeda2000@gmail.com>
2023-01-10 15:51:43 +0100

    modal arreglado, queda soso :/

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Recipe } from '../models/recipe';
-
+import {DomSanitizer} from '@angular/platform-browser';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.page.html',
@@ -12,14 +12,16 @@ export class DetailPage implements OnInit {
   @Input()
   recipe : Recipe | undefined;
   urlVideo: any;
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private domSanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.urlVideo=this.recipe?.link;
-    console.log(this.recipe?.link);
   }
   async closeModal() {
     await this.modalController.dismiss();
+  }
+  videoUrl(){
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(this.urlVideo);
   }
 
 }

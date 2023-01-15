3f857ed383c23b62e5991114bb4329f01898a907
Pablo Fernandez <pabloubeda2000@gmail.com>
2023-01-15 20:44:48 +0100

    tutorial de inicio de la app(added but not implemented yet) && arreglar estetica de la app && traducir titulo de la inspiracion && posibilidad de guardar la inspiracion en la coleccion personal

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+namespace Meals{
+  export interface Meal {
+    strMeal: string;
+    strMealThumb: string;
+    strYoutube: string;
+  }
+  export interface Meals {
+    meals: Meal[];
+  }
+}
+
 
 @Component({
   selector: 'app-tab4',
@@ -6,10 +20,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
+meal: Meals.Meal = {} as Meals.Meal;
+isYoutubeVideo: boolean = false;
+  constructor(private httpClient: HttpClient, private domSanitizer: DomSanitizer) {
 
-  constructor() { }
+
+
+   }
 
   ngOnInit() {
+    var object;
+    this.httpClient.get<Meals.Meals>('https://www.themealdb.com/api/json/v1/1/random.php').subscribe(data => {
+        object = data;
+        this.meal = object.meals[0];
+        if(this.meal.strYoutube != null){
+          this.isYoutubeVideo = true;
+          this.meal.strYoutube = this.meal.strYoutube.replace("watch?v=", "embed/");
+        }
+    });
   }
-
+  ionViewDidEnter(){
+    this.ngOnInit();
+  }
+  videoUrl(){
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(this.meal.strYoutube);
+  }
+  handleRefresh(event: any) {
+    setTimeout(() => {
+      this.ngOnInit();
+      event.target.complete();
+    }, 2000);
+  }
 }
